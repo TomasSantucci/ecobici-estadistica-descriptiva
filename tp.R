04a4e70443899a6496ffda7cd3d00bb40ef8776d
Tomás Santucci <tomisantucci@gmail.com>
2022-04-16 23:11:46 -0300

    Added new tables and graphs for other variables

--- tp.R
+++ tp.R
@@ -3,6 +3,7 @@
 # Integrantes: Tomas Santucci y Fernando Silva
 # ------------------------------------------------
 
+library(knitr)
 # vemos nuestro lugar de trabajo
 getwd()
 # seteamos nuestro lugar de trabajo y luego mostramos el lugar del trabajo
@@ -19,6 +20,7 @@ bicicletas <- merge(usuarios, recorridos, all = TRUE)
 View(bicicletas)
 View(usuarios)
 
+# GENERO
 # Utilizamos un grafico Barplot para representar los generos de cada usuario:
 
 genero <- table(usuarios$genero_usuario)[2:4]
@@ -33,7 +35,7 @@ barplot(
   col = c("blue","green","red")
 )
 
-
+# EDAD
 # Grafico para la edad un histograma
 edad <- usuarios$edad_usuario
 breakpoints <- c(15,20,25,30,35,40,45,50,55,60,72)
@@ -63,17 +65,43 @@ plot(x,y,type="l", xlim = c(15,72), main = titulo,
 for (i in seq(0,1,by=0.2)) abline(a=i,b=0,lty=2,lwd=0.5)
 
 
+# ESTACION DE ORIGEN
 
-# Usuarios
-table(bicicletas$id_usuario)
-table(usuarios$genero_usuario)
-table(usuarios$edad_usuario)
+frecAbs <- table(bicicletas$direccion_estacion_origen)
+frecAbs <- sort(frecAbs, decreasing = TRUE)
+frecAbs <- frecAbs[1:15]
+frecRel <- frecAbs/sum(frecAbs)
+frecAbsAcum <- cumsum(frecAbs)
+frecRelAcum <- cumsum(frecRel)
+frecRel <- round(frecRel, digits = 4)
+frecRelAcum <- round(frecRelAcum, digits = 4)
+tabla <- cbind(frecAbs,frecRel,frecAbsAcum,frecRelAcum)
+kable(tabla, caption = "Tabla de frecuencias")
 
-# Bicicletas
-table(bicicletas$direccion_estacion_origen)
-table(bicicletas$direccion_estacion_destino)
-table(bicicletas$duracion_recorrido)
-table(bicicletas$distancia)
-table(bicicletas$dia)
+barplot(
+  frecAbs,
+  horiz = TRUE,
+  las = 1,
+  xlim = c(0,25),
+  cex.names = 0.5,
+  col = "pink",
+)
 
-table(bicicletas$id_usuario, bicicletas$genero_usuario)
+# DISTANCIA
+
+distancia <- bicicletas$distancia/1000
+summary(distancia)
+hist(distancia,breaks = c(0,3,5,7,10,12,111), right = TRUE)
+boxplot(distancia, horizontal = TRUE, outline = FALSE,
+        xlab = "Distancia en kilometros", boxfill = "pink")
+
+#distancia[distancia>40000]
+
+#DURACION
+
+duracion <- bicicletas$duracion_recorrido/60
+summary(duracion)
+hist(duracion,breaks = c(0,10,20,30,45,60,100,500), right = TRUE)
+boxplot(duracion, horizontal = TRUE, outline = TRUE,
+        xlab = "Duracion en minutos", boxfill = "pink")
+
