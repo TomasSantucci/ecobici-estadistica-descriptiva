479d0db02534c3205f28323cf9360ebee2334ba0
Tomás Santucci <tomisantucci@gmail.com>
2022-04-20 13:51:57 -0300

    Refactor

--- tp.R
+++ tp.R
@@ -3,48 +3,63 @@
 # Integrantes: Tomas Santucci y Fernando Silva
 # ------------------------------------------------
 
+# Librerias
 library(knitr)
-# vemos nuestro lugar de trabajo
+
+
 getwd()
-# seteamos nuestro lugar de trabajo y luego mostramos el lugar del trabajo
 
 # leemos el csv de recorridos
 recorridos <- read.csv("./recorridos6.csv")
+
 # leemos el csv de usuarios
-usuarios <- read.csv("usuarios6.csv")
+usuarios <- read.csv("./usuarios6.csv")
 
 # combinamos los csv
 bicicletas <- merge(usuarios, recorridos, all = TRUE)
 
-# mostramos la informacion de las bicicletas
-View(bicicletas)
-View(usuarios)
+# ====================================================
 
-# GENERO
-# Utilizamos un grafico Barplot para representar los generos de cada usuario:
+# VARIABLE GENERO
+# Utilizamos un grafico de torta para representar 
+# el porcentaje de usuarios que utilizan el sistema
+# segun su genero
 
-genero <- table(usuarios$genero_usuario)[2:4]
+var_genero <- function(){
+  genero <- table(usuarios$genero_usuario)[2:4]
+  
+  label <- c("Femenino","Otro","\n\nMasculino")
+  porcentajes <- round((genero/sum(genero))*100, digits = 2)
+  label <- paste(label,"\n\t",porcentajes,"%")
+  color <- c("#FFC09F","#A0CED9","#FFEE93")
+  
+  pie(
+    genero,
+    labels = label,
+    col = color
+  )
+}
 
-nom <- c("Femenino","Otro","Masculino")
-perc <- round((genero/sum(genero))*100,digits = 2)
-nom <- paste(nom,"\n\t",perc,"%")
+var_genero()
 
-pie(
-  genero,
-  labels = nom,
-  col = c("blue","green","red")
-)
+#=================================================
 
-# EDAD
-# Grafico para la edad un histograma
+# VARIABLE EDAD
+# Utilizamos un grafico de histograma y graficos de poligonos de frecuencia
+# para representar la variacion de edad de los usuarios del sistema
+
 edad <- usuarios$edad_usuario
 breakpoints <- c(15,20,25,30,35,40,45,50,55,60,72)
+
 titulo <- "EDAD DE LOS USUARIOS DE ECOBICI\nCABA 2020"
 
 # Histograma
+
 hist(edad,breaks = breakpoints, right = TRUE,
      xlim = c(10,80), ylim = c(0,0.05), xlab = "Edad", ylab = "Densidad",
      main = titulo, col = "pink")
+
+
 
 rng_edad <- cut(edad, breakpoints, right = TRUE)
 tabla_rng_edad <- table(rng_edad)
@@ -58,20 +73,28 @@ plot(x,y,type="l", xlim = c(15,72), ylim = c(0,0.25),
      ylab = "Frecuencia relativa", xlab = "Edad",lwd = 1.5)
 for (i in seq(0,0.25,by=0.05)) abline(a=i,b=0,lty=2,lwd=0.5)
 
+
 # Poligono acumulativo
 y = cumsum(y)
 plot(x,y,type="l", xlim = c(15,72), main = titulo,
      ylab = "Frecuencia relativa acumulada", xlab = "Edad", lwd = 1.5)
 for (i in seq(0,1,by=0.2)) abline(a=i,b=0,lty=2,lwd=0.5)
 
+# ============================================
 # DIAS
+# Para representar la frecuencia de uso de las bicicletas
+# segun el dia de la semana utilizamos un barplot
 
 semana <- table(recorridos$dia)
 semana <- semana[order(c(7,4,1,2,3,6,5))]
 
-barplot(semana, ylim = c(0,150))
+barplot(semana, ylim = c(0,150), col = c("lightblue"))
+
+#===============================================
 
 # ESTACION DE ORIGEN
+# Representamos la estacion de origen con un barplot
+# y adicionalmente mostramos una tabla de las frecuencias del mismo
 
 frecAbs <- table(bicicletas$direccion_estacion_origen)
 frecAbs <- sort(frecAbs, decreasing = TRUE)
@@ -81,6 +104,7 @@ frecAbsAcum <- cumsum(frecAbs)
 frecRelAcum <- cumsum(frecRel)
 frecRel <- round(frecRel, digits = 4)
 frecRelAcum <- round(frecRelAcum, digits = 4)
+
 tabla <- cbind(frecAbs,frecRel,frecAbsAcum,frecRelAcum)
 kable(tabla, caption = "Tabla de frecuencias")
 
@@ -93,7 +117,11 @@ barplot(
   col = "pink",
 )
 
+#==========================================
+
 # DISTANCIA
+# vamos a mostrar la frecuencia de la variable distancia
+# con un boxplot
 
 distancia <- bicicletas$distancia/1000
 distancia <- distancia[distancia<20]
@@ -101,15 +129,23 @@ summary(distancia)
 boxplot(distancia, horizontal = TRUE, outline = TRUE,
         xlab = "Distancia en kilometros", boxfill = "pink")
 
+#===============================================
 
-#DURACION
+# DURACION
+# vamos a mostrar la frecuencia de la variable 
+# Duracion con un Boxplot
 
 duracion <- bicicletas$duracion_recorrido/60
 summary(duracion)
 boxplot(duracion, horizontal = TRUE, outline = FALSE,
         xlab = "Duracion en minutos", boxfill = "pink")
 
+# ========================================
+
 # CANTIDAD DE VIAJES POR USUARIO
+# Vamos a mostrar la cantidad de viajes por usuarios
+# con un grafico de baras
 
 viajes <- table(bicicletas$id_usuario)
-plot(table(viajes), ylim())
+plot(table(viajes), ylim = c(0,100), xlab = c("Viajes") ,ylab = c("Cantidad De Viajes"))
+
